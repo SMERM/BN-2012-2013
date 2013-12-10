4ee4aeb91fffce978e2ec9e16b7772114fdc9fbf
Giuseppe <giuseppe@Giuseppes-MacBook-Pro.local>
2013-12-11 00:14:06 +0100

    First Lesson

---
+++ lex01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main()
+    {
+    printf ("ciao stronzi!\n");
+    printf ("i tipi naturali sono:\n");
+    printf ("int = numeri interi riferiti all'architettura della macchina\n");
+    printf ("float = virgola mobile composto da [+ mantissa] [+ esponente]\n");
+    printf ("double = il doppio di float\n");
+    }
