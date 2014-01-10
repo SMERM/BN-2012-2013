7d3cfb038773890f0764c9428908586835dcb11d
Giuseppe <giuseppe@Giuseppes-MacBook-Pro.local>
2014-01-10 14:57:14 +0100

    lez 03 10 gen 2014

---
+++ PD-PLUGINS/simplest.c
@@ -0,0 +1,14 @@
+/*
+//  Simplest.c
+//  
+//
+//  Created by Giuseppe Silvi on 10/01/14.
+//
+*/
+
+#include <m_pd.h>
+
+void simplest_setup ()
+{
+    post("Ciao core! dalla funzione simplest");
+}
---
+++ lez03_pd.c
@@ -0,0 +1,13 @@
+/*
+//  lez03_pd.c
+//  
+//
+//  Created by Giuseppe Silvi on 10/01/14.
+//
+*/
+
+#include <stdio.h>
+
+main()
+{
+}
