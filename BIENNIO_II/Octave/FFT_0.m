6eb5d61a847c734c16e7c79bb4cb0d07831732f8
Giuseppe <giuseppe@Giuseppes-MacBook-Pro.local>
2014-02-21 10:17:51 +0100

    fft

---
+++ BIENNIO_II/Octave/FFT_0.m
@@ -0,0 +1,46 @@
+fc = 44100;
+sinc = 1/fc;
+
+dur = 0.65;
+
+t = [0:sinc:dur-sinc];
+
+winsize = 1024;
+binsize = fc/winsize;
+h = 1/2-1/2*cos(2*pi*(1/winsize)*[0:winsize-1]);
+
+amps = [0.8 0.3 0.1];
+%freqs = [binsize*13 binsize*66 binsize*127];
+freqs = [1250 5120 11036];
+fasi = [0 pi/4 pi/3];
+
+y0 = amps(1)*cos(2*pi*freqs(1)*t+fasi(1));
+y1 = amps(2)*cos(2*pi*freqs(2)*t+fasi(2));
+y2 = amps(3)*cos(2*pi*freqs(3)*t+fasi(3));
+
+y = y0.+y1.+y2;
+
+F = [0:binsize:fc-binsize];
+
+YDFTmag = zeros(size(F));
+YDFTfasi = zeros(size(F));
+
+for (k=1:winsize)
+	anal = exp(-i*2*pi*F(k)*t(1:winsize));
+	siganal = anal.*y(1:winsize).*h;
+	analsum = sum(siganal);
+	YDFTmag(k) = (4*abs(analsum))/(winsize);
+	YDFTfasi(k) = arg(analsum);
+end
+
+YFFT = fft(y(1:winsize).*h, winsize);
+YFFTmag = (4*abs(YFFT)/winsize);
+YFFTfasi = arg(YFFT);
+
+figure(1)
+subplot(2,1,1)
+stem(F,YDFTmag);
+axis([0 fc/2 0 1])
+subplot(2,1,2)
+stem(F,YFFTmag);
+axis([0 fc/2 0 1])
